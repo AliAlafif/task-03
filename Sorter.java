6c624a1d15cdf6057ff9d7f5e0192eab634283f0
aliafif <aliafif6001@gmail.com>
2023-02-10 20:43:39 +0300

    create and implement a selectionsort method

--- Sorter.java
+++ Sorter.java
@@ -2,4 +2,21 @@ public class Sorter{
     public static void main(String[] args) {
         System.out.println("ALi Alafif");
     }
+
+public int[] SelectionSort(int[] array){
+for(int i=0;i<array.length;i++){
+    int min =i;
+    for(int j=i+1;j<array.length;j++){
+        if(array[j]<array[min]){
+            min=j;
+        }
+    }
+    if(i!= min){
+        int temp=array[i];
+        array[i]=array[min];
+        array[min]=temp;
+    }
+}
+return array;
+}
 }
