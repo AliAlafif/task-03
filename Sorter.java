665b54b7bce96f61c5a2f5f5d5f8aee40d2dcd78
aliafif <aliafif6001@gmail.com>
2023-02-13 18:00:58 +0300

    implement a method called insertionSort(Array)

--- Sorter.java
+++ Sorter.java
@@ -5,6 +5,8 @@ public class Sorter{
         System.out.println("ALi Alafif");
         int[] a={5,6,2,4,6};
         System.out.println(Arrays.toString(SelectionSort(a)));
+        System.out.println(Arrays.toString(insertionSort(a)));
+
     }
 
 public static int[] SelectionSort(int[] array){
@@ -26,5 +28,17 @@ return array;
 
 public static int[] sort(int[] array){
     return SelectionSort(array);
+}
+
+public static int[] insertionSort(int[] array){
+    for(int i=1;i<array.length;i++){
+        int temp=array[i];
+        int j;
+        for(j=i-1;j>=0 && temp<array[j];j--){
+            array[j+1]=array[j];
+        }
+        array[j+1]=temp;
+    }
+    return array;
 }
 }
